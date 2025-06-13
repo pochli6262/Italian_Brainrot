9a44e2d98f92ed4d5b1b56977ea855cb889a89f8
jason <pochli6262@gmail.com>
2025-06-13 17:15:52 +0800

    Initial Commit

---
+++ Italian_Brainrot/Italian_BrainrotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Italian_BrainrotApp.swift
+//  Italian_Brainrot
+//
+//  Created by 廖柏鈞 on 2025/6/13.
+//
+
+import SwiftUI
+
+@main
+struct Italian_BrainrotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
